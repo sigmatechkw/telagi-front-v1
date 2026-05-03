51e4ccd51db070f667c8e9cf783f1810d4968d09
SaadSleem80 <saadsleem80@gmail.com>
2026-05-04 01:34:26 +0300

    feat: add approved button on edit ads form.

--- src/pages/ads/edit/[id].js
+++ src/pages/ads/edit/[id].js
@@ -9,32 +9,33 @@ import { useSelector } from 'react-redux'
 import { fetchAdsDetails } from 'src/components/Ads/Details/adsDetailsServices'
 import AdsForm from 'src/components/Ads/AdsForm'
 import { nanoid } from 'nanoid'
+import { useQuery } from '@tanstack/react-query'
 
 const defaultValues = {
-    title: "",
-    description: "",
-    expiration_period: "",
-    expiration_date: "",
-    price : "",
-    phone: "",
-    whatsapp: "",
-    views: "",
-    active: false,
-    sold: false,
-    approved: false,
-    featured: false,
-    featured_start_date: false,
-    featured_end_date: false,
-    home_end_date: false,
-    inside_end_date: false,
-    top_categories_end_date : false,
-    status: false,
-    country_id: false,
-    category_id: false,
-    user_id: false,
-    image : "",
-    video : "",
-    attributes : []
+  title: '',
+  description: '',
+  expiration_period: '',
+  expiration_date: '',
+  price: '',
+  phone: '',
+  whatsapp: '',
+  views: '',
+  active: false,
+  sold: false,
+  approved: false,
+  featured: false,
+  featured_start_date: false,
+  featured_end_date: false,
+  home_end_date: false,
+  inside_end_date: false,
+  top_categories_end_date: false,
+  status: false,
+  country_id: false,
+  category_id: false,
+  user_id: false,
+  image: '',
+  video: '',
+  attributes: []
 }
 
 const AdsEdit = ({ type, id }) => {
@@ -50,7 +51,7 @@ const AdsEdit = ({ type, id }) => {
   const [videoSrc, setVideoSrc] = useState('')
   const [adVideo, setAdVideo] = useState('')
   const [content, setContent] = useState('')
-  const [category_id , setCategoryId] = useState('')
+  const [category_id, setCategoryId] = useState('')
 
   const {
     control,
@@ -62,85 +63,91 @@ const AdsEdit = ({ type, id }) => {
     formState: { errors }
   } = useForm({ defaultValues })
 
-    const testBase64 = src => {
+  const { data, refetch } = useQuery({
+    queryKey: ['fetchAdsDetails', id],
+    queryFn: () => fetchAdsDetails(id),
+    initialData: type
+  })
+
+  const testBase64 = src => {
     const base64Regex = /^(data:image\/[a-zA-Z]*;base64,)?([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/
 
     return base64Regex.test(src)
   }
-  
+
   const onSubmit = data => {
     setLoading(true)
 
-    data.country_id = data.country_id?.id ? data.country_id?.id : 1;
-    data.category_id = data.category_id;
-    data.user_id = data.user_id?.id;
+    data.country_id = data.country_id?.id ? data.country_id?.id : 1
+    data.category_id = data.category_id
+    data.user_id = data.user_id?.id
 
-    data.active = data.active ? 1 : 0;
-    data.sold = data.sold ? 1 : 0;
-    data.approved = data.approved ? 1 : 0;
-    data.featured = data.featured ? 1 : 0;
-    
-    if(!imgSrc){ 
-      delete data.image;
-    }else{ 
-      if(type.image == imgSrc){
-        delete data.image;
-      }else{ 
-        data.image = imgSrc;
+    data.active = data.active ? 1 : 0
+    data.sold = data.sold ? 1 : 0
+    data.approved = data.approved ? 1 : 0
+    data.featured = data.featured ? 1 : 0
+
+    if (!imgSrc) {
+      delete data.image
+    } else {
+      if (type.image == imgSrc) {
+        delete data.image
+      } else {
+        data.image = imgSrc
       }
     }
 
-    if(!videoSrc){ 
-      delete data.video;
-    }else{ 
-      delete data.video;
-      if(getValues('video')?.url == videoSrc){
-      }else{ 
-        data.video = videoSrc;
-        data.deleted_videos_ids = [getValues('video')?.id];
+    if (!videoSrc) {
+      delete data.video
+    } else {
+      delete data.video
+      if (getValues('video')?.url == videoSrc) {
+      } else {
+        data.video = videoSrc
+        data.deleted_videos_ids = [getValues('video')?.id]
       }
     }
 
-    if(!imgsArr){ 
-      delete data.imgsArr;
-    }else{ 
-      if(type.images == imgsArr){
-        delete data.images;
-      }else{ 
-        data.images = imgsArr;
-        data.deleted_images_ids = type.images.map(image => image?.id);
+    if (!imgsArr) {
+      delete data.imgsArr
+    } else {
+      if (type.images == imgsArr) {
+        delete data.images
+      } else {
+        data.images = imgsArr
+        data.deleted_images_ids = type.images.map(image => image?.id)
       }
     }
 
-    if(!type.expiration_date){ 
+    if (!type.expiration_date) {
       delete data.expiration_date
     }
 
-    if(!type.featured_start_date){ 
+    if (!type.featured_start_date) {
       delete data.featured_start_date
     }
 
-    if(!type.featured_end_date){
+    if (!type.featured_end_date) {
       delete data.featured_end_date
     }
 
     //check for the new or updated attributes
-    const attributes = [];
-    data.attributes.forEach((dataAttribute) => { 
-        const oldAttributeSet = attributes.find((e) => e.attribute_set_id == dataAttribute.attribute_set_id);
-        if(oldAttributeSet) { 
-          oldAttributeSet.value.push(dataAttribute.value);
-        }else{ 
-        }
-        attributes.push({attribute_set_id : dataAttribute.attribute_set_id , value : [dataAttribute.value]});
-    });
-  
-    data.attributes = attributes;
-    
+    const attributes = []
+    data.attributes.forEach(dataAttribute => {
+      const oldAttributeSet = attributes.find(e => e.attribute_set_id == dataAttribute.attribute_set_id)
+      if (oldAttributeSet) {
+        oldAttributeSet.value.push(dataAttribute.value)
+      } else {
+      }
+      attributes.push({ attribute_set_id: dataAttribute.attribute_set_id, value: [dataAttribute.value] })
+    })
+
+    data.attributes = attributes
+
     axios
       .post(`${process.env.NEXT_PUBLIC_API_KEY}update-ads/${id}`, data, {
         headers: {
-          'Accept': 'application/json',
+          Accept: 'application/json',
           'Content-Type': 'multipart/form-data',
           Authorization: auth.token
         }
@@ -157,7 +164,8 @@ const AdsEdit = ({ type, id }) => {
       })
   }
 
-  const fetchAdsDetails = async () => {
+  const populateForm = async (type) => {
+    setValue('id', type.id)
     setValue('title', type.title)
     setValue('description', type.description)
     setValue('expiration_period', type.expiration_period)
@@ -166,10 +174,10 @@ const AdsEdit = ({ type, id }) => {
     setValue('phone', type.phone)
     setValue('whatsapp', type.whatsapp)
     setValue('views', type.views)
-    setValue('active', type.active == 1? true: false )
-    setValue('sold', type.sold == 1? true: false)
-    setValue('approved', type.approved == 1? true: false)
-    setValue('featured', type.featured == 1? true: false)
+    setValue('active', type.active == 1 ? true : false)
+    setValue('sold', type.sold == 1 ? true : false)
+    setValue('approved', type.approved == 1 ? true : false)
+    setValue('featured', type.featured == 1 ? true : false)
     setValue('featured_start_date', type.featured_start_date)
     setValue('featured_end_date', type.featured_end_date)
     setValue('home_end_date', type.home_end_date)
@@ -181,29 +189,29 @@ const AdsEdit = ({ type, id }) => {
     setCategoryId(type.category?.id)
     setValue('user_id', type.user)
     setValue('image', type.image)
-    setImgSrc(type.image);
+    setImgSrc(type.image)
     setValue('images', type.images)
     setImgsArr(type.images)
     setValue('video', type.video[0])
     setAdVideo(type.video[0]?.url)
 
-    let attributes = [];
-    if(type.ad_attributes.length > 0) { 
-      await type.ad_attributes.forEach((e) => { 
-            e.attributes.forEach((attrItem) => { 
-              const attr = {id: nanoid(), attribute_set_id : e.attribute_set?.id , value : attrItem?.id};
-              attributes.push(attr);
-            })
-      });
+    let attributes = []
+    if (type.ad_attributes.length > 0) {
+      await type.ad_attributes.forEach(e => {
+        e.attributes.forEach(attrItem => {
+          const attr = { id: nanoid(), attribute_set_id: e.attribute_set?.id, value: attrItem?.id }
+          attributes.push(attr)
+        })
+      })
     }
     setValue('attributes', attributes)
   }
 
   useEffect(() => {
-    if (id) {
-        fetchAdsDetails()
+    if (id || data) {
+      populateForm(data || type)
     }
-  }, [id])
+  }, [id, data])
 
   return (
     <Card>
@@ -229,6 +237,8 @@ const AdsEdit = ({ type, id }) => {
         errors={errors}
         title={t('ads_edit')}
         loading={loading}
+        getValues={getValues}
+        refetch={refetch}
       />
     </Card>
   )
